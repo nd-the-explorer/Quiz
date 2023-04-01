1e46e236b2b8aeff95320de9d102eb9dc5105027
nd-the-explorer <81695788+nd-the-explorer@users.noreply.github.com>
2023-04-01 15:50:28 +0530

    final code

---
+++ admin/files/footer.php
@@ -0,0 +1,10 @@
+  <footer class="footer">
+    <div class="container-fluid">
+      <div class="copyright">
+        
+        <script>
+          
+        </script>
+      </div>
+    </div>
+  </footer>
